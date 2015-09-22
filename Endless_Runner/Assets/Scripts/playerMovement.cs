8238cc446cc74fef3b0af5013b7b946ae361f2af
RRH707 <roushan_hausil@hotmail.com>
2015-09-22 11:10:14 +0200

    first commit

---
+++ Endless_Runner/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*/Author: Rishaad Hausil /*/
+
+public class playerMovement : MonoBehaviour
+{
+    
+    public Transform groundCheck;
+    public float rdius;
+    public LayerMask ground;
+
+    private bool _onGround;
+    private bool _doubleJumped;
+    private float _jumpHeight = 8;
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if(_onGround)
+        {
+            _doubleJumped = false;
+        }
+
+        _onGround = Physics2D.OverlapCircle(groundCheck.transform.position, rdius, ground);
+
+        if(Input.GetKeyDown(KeyCode.Space) && _onGround)
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !_doubleJumped && !_onGround)
+        {
+            Jump();
+            _doubleJumped = true;
+        }
+      }
+    private void Jump()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, _jumpHeight);
+    }
+}
